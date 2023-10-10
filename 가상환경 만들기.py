127e29cf4513f29f50528946e2c0b2f19707443c
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-10-10 13:27:20 +0900

    Update 가상환경 만들기.py

--- 가상환경 만들기.py
+++ 가상환경 만들기.py
@@ -3,15 +3,27 @@
 # 이를 해결하기 위해서 가상환경을 만들어 각 프로젝트마다 패키징을 한다. 
 # 가상환경 : 독립된 공간을 만들어 주는 기능 
 
-# 가상환경 폴더 생성 
+# 1. 가상환경 폴더 생성 
 # python -m venv 가상환경이름
 # powershell 기준입니다. 
+# 만약에 실행이 되지 않는다면 powershell을 관리자 권한으로 실행후 다시 실행합니다. 
+# Set-ExecutionPolicy Unrestricted -> 코드 실행 
 
 python -m venv env
 cd env
 ./Scrpits/Activate.ps1 
 # (env) PS C:\project\example>
 
-# 만약에 실행이 되지 않는다면 powershell을 관리자 권한으로 실행후 다시 실행합니다. 
-# Set-ExecutionPolicy Unrestricted -> 코드 실행 
+# 2. 이 상태에서 패키지를 설치한다면 (가상환경명)\Lib\site-packages 안에 패키지가 저장됩니다
 
+# 3. 패키지 목록 관리하기 
+# 가상 환경에 설치된 패키지는 목록을 저장해 두었다가 나중에 다시 설치할 수 있습니다.
+
+# pip freeze로 저장하기 
+pip freeze > requirements.txt
+
+# pip install로 설치하기 
+pip install -r requirements.txt
+
+# pip uninstall로 삭제하기 
+pip uninstall -r requirements.txt
